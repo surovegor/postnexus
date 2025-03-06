644c2f723f4dfc7bf89d2b996108574847f6660d
surovegor <surovpro@gmail.com>
2025-03-06 15:52:33 +0400

    feat: add /start command with welcome message and keyboard layout

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();  
 
 const { Telegraf } = require('telegraf'); 
-const { message } = require('telegraf/filters');
 const token = process.env.TELEGRAM_TOKEN;
 
 if (!token) {
@@ -10,8 +9,41 @@ if (!token) {
 }
 
 const bot = new Telegraf(token); 
-bot.start((ctx) => { 
-  ctx.reply('Привет! Я твой Telegram-бот.'); 
+
+const welcomeMessage = 
+`*PostNexus* — это простой и удобный бот для отложенного постинга, поддерживающий работу с любыми форматами контента.
+
+*Бот позволяет:*
+
+✔️Планировать выход публикаций в ваших каналах
+
+✔️Автоматически удалять их по расписанию
+
+✔️Создавать и настраивать посты любого формата
+
+✔️И многое другое`;
+
+const menuKeyboard = { 
+  reply_markup: {
+    keyboard: [
+      ['Добавить канал'],
+      ['Мои каналы'] 
+    ],
+    resize_keyboard: true, // Изменение размера клавиатруы для кнопок
+    one_time_keyboard: false // Сокрытие клавиатуры после нажатия
+  }
+};
+
+bot.start((ctx) => { // Обработчик для кнопки СТАРТ (/start)
+  ctx.reply(welcomeMessage, { parse_mode: 'Markdown', ...menuKeyboard }); 
+});
+
+bot.hears('Добавить канал', (ctx) => { // Обработчик для кнопки "Добавить канал"
+  ctx.reply('Вы выбрали "Добавить канал". Введите название канала:');
+});
+
+bot.hears('Мои каналы', (ctx) => { // Обработчик для кнопки "Мои каналы"
+  ctx.reply('Вы выбрали "Мои каналы". Вот список ваших каналов:');
 });
 
 bot.on('message', (ctx) => {
